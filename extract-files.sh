8243911ada035c4903b7da714066df83b178d060
Vladimir Oltean <olteanv@gmail.com>
2018-12-27 05:11:19 +0200

    msm8937-common: introduce kang mode
    Also do the following:
    * Fix passing of arguments longer than one word, such as --section "Hello world"
    
    Wight554:
    * Small cosmetic changes
    * Fix some shellcheck warnings
    
    Change-Id: I917ed382a8c0e33ba7709151f2743a723dc50617
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -18,44 +18,67 @@
 
 set -e
 
-INITIAL_COPYRIGHT_YEAR=2017
+DEVICE_COMMON=msm8937-common
+VENDOR=motorola
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="${MY_DIR}"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
+
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -n | --no-cleanup )
+            CLEAN_VENDOR=false
+            ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
+    esac
+    shift
+done
+
+if [ -z "${SRC}" ]; then
+    SRC="adb"
+fi
 
 # Initialize the helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEAN_VENDOR}"
 
-# Copyright headers and guards
-write_headers "cedric montana owens perry"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
+        "${KANG}" --section "${SECTION}"
 
-# The standard common blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt true
+# Fixup common blobs
+COMMON_BLOB_ROOT="${LINEAGE_ROOT}/vendor/${VENDOR}/${DEVICE_COMMON}/proprietary"
 
-# We are done!
-write_footers
+patchelf --replace-needed libui.so libui-v27.so "${COMMON_BLOB_ROOT}/vendor/lib64/lib-imsvideocodec.so"
 
-if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
+if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false
-    DEVICE_BRINGUP_YEAR=$INITIAL_COPYRIGHT_YEAR
+    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
 
-    # Copyright headers and guards
-    write_headers
+    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" \
+            "${KANG}" --section "${SECTION}"
 
-    # The standard device blobs
-    write_makefiles "$MY_DIR"/../$DEVICE/proprietary-files.txt true
-
-    # We are done!
-    write_footers
+    device_blob_fixup
 fi
+
+source "${MY_DIR}/setup-makefiles.sh"
