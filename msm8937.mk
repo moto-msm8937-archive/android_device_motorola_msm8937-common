d060e4e7ae6a4b804551c4b872d862beb2bc3354
Vachounet <vachounet@live.fr>
2019-01-23 16:16:30 +0100

    msm8937-common: switch to binderized vibrator service
    Change-Id: I24cc8998aa0b38e090fd40515795bb4f313b081b

--- msm8937.mk
+++ msm8937.mk
@@ -329,7 +329,7 @@ PRODUCT_PACKAGES += \
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service.msm8937
 
 # VNDK
 PRODUCT_PACKAGES += \
