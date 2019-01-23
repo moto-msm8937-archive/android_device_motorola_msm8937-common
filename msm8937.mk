d6a02af4c130752eb4da4062925f5dd08b0900f6
Vachounet <vachounet@live.fr>
2019-01-23 16:18:19 +0100

    msm8937-common: switch to binderized light service
    Change-Id: I74e06a2c112d9a447ab86c50c13fab69687b4454

--- msm8937.mk
+++ msm8937.mk
@@ -230,7 +230,7 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
+    android.hardware.light@2.0-service.msm8937 \
     lights.msm8937
 
 # Media
