fe574f1e21af2a5967d779ba15a97573d4ef9f69
AsD Monio <asdmonio@gmail.com>
2019-03-09 22:15:14 +0100

    msm8937-common: Build WPA Supplicant HAL
    Change-Id: Id0919277fa798d6377b0d4d1d9d7387245da809a

--- msm8937.mk
+++ msm8937.mk
@@ -333,6 +333,7 @@ PRODUCT_PACKAGES += \
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
+    android.hardware.wifi.supplicant@1.0 \
     hostapd \
     libqsap_sdk \
     libwpa_client \
