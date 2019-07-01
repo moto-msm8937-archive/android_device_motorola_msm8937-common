b8d0d4d584fca98f8af278cf51d89556683998e9
Sludke <46661484+xdsurvivor@users.noreply.github.com>
2019-07-01 02:09:40 -0300

    msm8937-common: hardware-moto-sensors: Move sensorhub firmware path to /vendor
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ie6879ba6c3c9d85cb7ab9f58dd242d9235bc0442

--- hardware-moto-sensors/motosh_bin/motosh.cpp
+++ hardware-moto-sensors/motosh_bin/motosh.cpp
@@ -290,7 +290,7 @@ int stm_getFwFile(char *fileName, FirmwareType type) {
             ret = snprintf(fileName, STM_MAX_PATH, "/data/misc/sensorhub/sensorhubfw.bin");
             break;
         case FW_STOCK:
-            ret = snprintf(fileName, STM_MAX_PATH, "/system/etc/firmware/sensorhubfw.bin");
+            ret = snprintf(fileName, STM_MAX_PATH, "/vendor/firmware/sensorhubfw.bin");
             break;
         default:
             return -1; // Invalid firmware type
