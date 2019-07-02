cf1a8c55f1ea0c4d95783d1259c3ead8101525d5
JarlPenguin <jarlpenguin@outlook.com>
2019-07-02 18:10:24 +0300

    msm8937-common: build vendor version of HIDL blobs
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>
    Change-Id: Ia22659217b46d96f332db1f922056b05074c706b

--- msm8937.mk
+++ msm8937.mk
@@ -164,8 +164,8 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0 \
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0.vendor \
     android.hidl.manager-V1.0-java
 
 # IDC
