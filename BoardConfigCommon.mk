f934aa74e5a70848b352c4b6323c233be00cdcbe
Jarl-Penguin <jarlpenguin@outlook.com>
2019-08-12 02:28:34 +0300

    msm8937-common: decommonize some flags
    * Needed for lunch to work properly
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,13 +20,10 @@ TARGET_RECOVERY_DEVICE_DIRS := \
     $(COMMON_PATH) \
     $(DEVICE_PATH)
 
-# Architecture
-TARGET_ARCH := arm64
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+# Arch
 TARGET_CPU_VARIANT := cortex-a53
 
 TARGET_2ND_ARCH := arm
@@ -34,9 +31,6 @@ TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
-
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
 # Board
 TARGET_BOARD_PLATFORM := msm8937
@@ -51,6 +45,8 @@ TARGET_HW_DISK_ENCRYPTION := true
 TW_INCLUDE_CRYPTO := true
 
 # Kernel
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 vmalloc=400M androidboot.bootdevice=7824900.sdhci movablecore=160M androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_RAMDISK_OFFSET := 0x01000000
