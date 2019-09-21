108d0d716eec6b604c22624a62226c306a9935c4
flagersgit <flagersmail@gmail.com>
2019-09-20 21:32:25 -0400

    msm8937-common: decommonize display props

--- system_prop.mk
+++ system_prop.mk
@@ -103,22 +103,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.enable_hwc_vds=1 \
-    debug.egl.hw=0 \
-    debug.sf.hw=0 \
-    debug.sf.latch_unsignaled=1 \
     persist.hwc.mdpcomp.enable=true \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
     ro.opengles.version=196610 \
-    ro.sf.lcd_density=420 \
-    persist.debug.wfd.enable=1 \
-    sdm.debug.disable_skip_validate=1 \
-    debug.gralloc.enable_fb_ubwc=1 \
     persist.camera.preview.ubwc=0 \
     persist.camera.video.ubwc=0 \
-    persist.hwc.enable_vds=1 \
-    debug.sf.recomputecrop=0 \
     debug.enable.sglscale=1
 
 # FM
