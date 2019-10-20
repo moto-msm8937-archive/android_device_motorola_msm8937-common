LineageOS/android_device_xiaomi_msm8937-common

msm8937-common: Adapt for local LineageHW java overlays
Change-Id: Icb2b9810217f4b4d442ab83a16f455bd2ed82071

BoardConfigCommon.mk
@@ -131,7 +131,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
 TARGET_PROVIDES_LIBLIGHT := true
 
 # Lineage Hardware
-BOARD_HARDWARE_CLASS += $(VENDOR_PATH)/lineagehw
+JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(VENDOR_PATH)/lineagehw|**/*.java
 
 # Malloc
 MALLOC_SVELTE := true

moto-msm8937-archive/android_device_motorola_msm8937-common

BoardConfigCommon.mk
@@ -151,8 +151,7 @@ TARGET_PROVIDES_KEYMASTER := true
 TARGET_PROVIDES_LIBLIGHT := true
 
 # Lineage Hardware
-BOARD_HARDWARE_CLASS += \
-    $(COMMON_PATH)/lineagehw
+JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072