4c2746ac5ddaa15ca1fff46ce8c4963e86600510
JarlPenguin <jarlpenguin@outlook.com>
2020-01-04 00:56:36 +0300

    msm8937-common: Build SEPolicy
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -194,7 +194,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
